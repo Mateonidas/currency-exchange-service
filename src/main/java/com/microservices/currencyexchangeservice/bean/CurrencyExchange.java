9c9edbf5f0eb6396454eaa7494836d758c129179
mateonidas <teus.polec.other@gmail.com>
2022-04-13 10:19:53 +0200

    Add connection to database and JPA repository

--- src/main/java/com/microservices/currencyexchangeservice/bean/CurrencyExchange.java
+++ src/main/java/com/microservices/currencyexchangeservice/bean/CurrencyExchange.java
@@ -1,12 +1,26 @@
 package com.microservices.currencyexchangeservice.bean;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
+
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
+
+    @Column
     private BigDecimal conversionMultiple;
+
+    @Column
     private String environment;
 
     public CurrencyExchange() {
