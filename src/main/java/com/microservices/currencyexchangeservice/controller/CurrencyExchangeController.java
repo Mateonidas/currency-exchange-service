722cbc240b3aa9f51178d2aed76e9ecab437c777
mateonidas <teus.polec.other@gmail.com>
2022-04-15 12:36:50 +0200

    add connection to zipkin

--- src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.microservices.currencyexchangeservice.controller;
 
 import com.microservices.currencyexchangeservice.bean.CurrencyExchange;
 import com.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private CurrencyExchangeRepository currencyExchangeRepository;
 
@@ -19,6 +23,8 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
 //        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
         if (currencyExchange == null) {
